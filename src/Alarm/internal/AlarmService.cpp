ef7bb04f43808677fbccdb8326b6def7d16a1457
Sandro Pedrett <sandro.pedrett@hotmail.com>
2025-07-02 14:45:23 +0200

    Added some hints for Alarm Feature

--- src/Alarm/internal/AlarmService.cpp
+++ src/Alarm/internal/AlarmService.cpp
@@ -5,8 +5,9 @@
 AlarmService::AlarmService(Mediator& mediator, TimeProviderIfc const& timeProvider) :
     m_mediator {mediator},
     m_timeProvider {timeProvider},
+    m_currentTimestamp {0ULL},
     m_timerRunning {true},
-    m_timerThread {&AlarmService::processTime, this} {
+    m_timerThread {} {
 }
 
 AlarmService::~AlarmService() {
@@ -18,17 +19,30 @@ AlarmService::~AlarmService() {
 }
 
 void AlarmService::initialize() {
-    // TODO
+    // create and start timerThread
+    m_timerRunning = true;
+    m_timerThread = std::thread(&AlarmService::processTime, this);
 }
 
 void AlarmService::processTime() {
     while (m_timerRunning) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
+
         updateTimestamp();
+        checkAlarmConditions();
     }
 }
 
 void AlarmService::updateTimestamp() {
-    DateTime const currentTimestamp {m_timeProvider.getCurrentTimestamp()};
-    m_mediator.notify(TimeChangedEvent {currentTimestamp});
+    m_currentTimestamp = m_timeProvider.getCurrentTimestamp();
+    m_mediator.notify(TimeChangedEvent {m_currentTimestamp});
+}
+
+void AlarmService::checkAlarmConditions() {
+    // TODO Alarm Feature
+    // check if any alarm conditions (get alarms from AlarmPersistency)
+    // is true and notify mediator with AlarmActivatedEvent(alarm).
+    //
+    // Alarm can be snoozed/stopped with e.g. AlarmService::snooze(alarmId, 10min)
+    // which sends an AlarmDeactivedEvent(alarm, Reason::Snooze)
 }
