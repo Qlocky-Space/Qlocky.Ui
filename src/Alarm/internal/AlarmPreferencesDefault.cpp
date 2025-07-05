0567e97225938bb3e9566c0f5efe8c473bac43c6
Sandro Pedrett <4846634+sanore@users.noreply.github.com>
2025-07-05 09:18:10 +0200

    Extract Date/Time helper method

---
+++ src/Alarm/internal/AlarmPreferencesDefault.cpp
@@ -0,0 +1,4 @@
+#include "AlarmPreferencesDefault.h"
+
+std::string const AlarmPreferencesDefault::DEFAULT_TIMEZONE {"Europe/Zurich"};
+std::string const AlarmPreferencesDefault::DEFAULT_24H_FORMAT {"HH"};
--- src/Alarm/view/ClockCardViewModel.cpp
+++ src/Alarm/view/ClockCardViewModel.cpp
@@ -1,8 +1,9 @@
 #include "ClockCardViewModel.h"
 
 #include <iostream>
-#include <QDateTime>
 #include <QTimeZone>
+
+#include "internal/AlarmPreferencesDefault.h"
 
 ClockCardViewModel::ClockCardViewModel(Mediator& mediator, PingCommand& pingCommand, PreferencesServiceIfc& preferences) :
     QObject {nullptr},
@@ -14,25 +15,38 @@ ClockCardViewModel::ClockCardViewModel(Mediator& mediator, PingCommand& pingComm
 }
 
 void ClockCardViewModel::onTimeChanged(TimeChangedEvent const& event) {
-    static std::string const defaultTimeZone {"Europe/Zurich"};
+    QDateTime const dt {getLocalTime(event.getValue())};
 
-    QDateTime const unixTimestamp {QDateTime::fromSecsSinceEpoch(event.getValue(), QTimeZone::utc())};
-
-    auto& ctx = m_preferences.getContext("Alarm");
-    std::string const prefTimeZone {ctx.getString("TimeZone", defaultTimeZone)};
-    QTimeZone timeZone {prefTimeZone.c_str()};
-    QDateTime const dt {unixTimestamp.toTimeZone(timeZone)};
-
-    m_date = dt.toString("dddd, d. MMMM yyyy");
-
-    bool const showSeperator {(event.getValue() % 2U) == 0U};
-    if (showSeperator) {
-        m_time = dt.toString("HH:mm");
-    }
-    else {
-        m_time = dt.toString("HH mm");
-    }
+    m_date = formatDate(dt);
+    m_time = formatTime(dt);
 
     emit timeChanged();
     emit dateChanged();
+}
+
+QDateTime ClockCardViewModel::getLocalTime(DateTime const timestamp) {
+    auto& ctx = m_preferences.getContext("Alarm");
+    std::string const prefTimeZone {ctx.getString("TimeZone", AlarmPreferencesDefault::DEFAULT_TIMEZONE)};
+
+    QTimeZone const timeZone {prefTimeZone.c_str()};
+    QDateTime const unixTimestamp {QDateTime::fromSecsSinceEpoch(timestamp, QTimeZone::utc())};
+
+    return unixTimestamp.toTimeZone(timeZone);
+}
+
+QString ClockCardViewModel::formatTime(QDateTime const& time) {
+    auto& ctx = m_preferences.getContext("Alarm");
+    std::string const hFormat {ctx.getString("24HFormat", AlarmPreferencesDefault::DEFAULT_24H_FORMAT)};
+
+    bool const showSeperator {(time.toSecsSinceEpoch() % 2U) == 0U};
+    std::string format {};
+    format.append(hFormat);
+    format.append(showSeperator ? ':' : ' ');
+    format.append("mm");
+
+    return time.toString(format);
+}
+
+QString ClockCardViewModel::formatDate(QDateTime const& time) {
+    return time.toString("dddd, d. MMMM yyyy");
 }
