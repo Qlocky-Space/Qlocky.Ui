6e95c6061609f9cb9406e7f410b3ed9f2c95f491
Sandro Pedrett <4846634+sanore@users.noreply.github.com>
2025-08-05 21:40:52 +0200

    Moved some AppShell components to Device module

--- src/Platform/Device/api/DeviceModule.cpp
+++ src/Platform/Device/api/DeviceModule.cpp
@@ -1,7 +1,21 @@
 #include "DeviceModule.h"
 
+#include "ConfigurationRegistryIfc.h"
 #include "internal/DeviceInfoProvider.h"
+#include "QmlRegistryUtil.h"
+#include "view/AboutViewModel.h"
 
 void DeviceModule::registerExports(Injector& container) {
     container.install(boost::di::bind<DeviceInfoProviderIfc>().to<DeviceInfoProvider>());
+}
+
+void DeviceModule::registerQmlTypes() {
+    QmlRegistryUtil::qmlRegisterViewModel<AboutViewModel>(*this, "AboutViewModel");
+}
+
+void DeviceModule::onInitialize() {
+
+    auto configRegistry = resolve<ConfigurationRegistryIfc>();
+    configRegistry->registerProvider("about", ConfigurationMeta {"System", "About", "/qt/qml/Device/qml/About.qml"});
+    configRegistry->registerProvider("develop", ConfigurationMeta {"System", "Develop", "/qt/qml/Device/qml/Develop.qml"});
 }
--- src/Platform/Device/api/DeviceModule.h
+++ src/Platform/Device/api/DeviceModule.h
@@ -14,6 +14,16 @@ public:
      */
     void registerExports(Injector& container) final;
 
+    /**
+     * @see ModuleBase::registerQmlTypes
+     */
+    void registerQmlTypes() final;
+
+    /**
+     * @see ModuleBase::onInitialize
+     */
+    void onInitialize() final;
+
     DeviceModule() = default;
     ~DeviceModule() final = default;
 
